506c66f96f30c6a9e0ae36ef128d639fc54eeaf3
Frederik Polachowski <frederik.polachowski@tu-dortmund.de>
2023-09-15 14:26:10 +0200

    changed input device->2

--- reid_demo/start_pallet_det.py
+++ reid_demo/start_pallet_det.py
@@ -2,7 +2,7 @@ from aideck import Detector
 import time
 
 def main():
-    detector_pallet = Detector("aideck/models/yolov4-tiny.cfg", "aideck/models/yolov4-tiny_pallet.weights", "pallet_bb", "Bounding Box Publisher for pallets", video_device=0)
+    detector_pallet = Detector("aideck/models/yolov4-tiny.cfg", "aideck/models/yolov4-tiny_pallet.weights", "pallet_bb", "Bounding Box Publisher for pallets", video_device=2)
     # detector_palletBlock = Detector("aideck/models/yolov4-tiny.cfg", "aideck/models/yolov4-tiny_pallet_block.weights", "palletBlock_bb", "Bounding Box Publisher for pallet blocks")
 
     detector_pallet.start()
