a5f02c0ceed4799ebaf8e12bcac440b9e4c7f7fb
Frederik Polachowski <frederik.polachowski@tu-dortmund.de>
2024-01-31 16:26:29 +0100

    new version of drone demo with pid control

--- Drone.py
+++ Drone.py
@@ -4,21 +4,24 @@ import time
 from simple_pid import PID
 
 STEP_FLIGHT_TIME = 1.5
-MIN_HEIGHT = 0.2
-IMAGE_CAPTURE_MIN_AREA = 11000
+MIN_HEIGHT = 0.15
+IMAGE_CAPTURE_MIN_AREA = 14000
 LOG_TRACKING = True
 AREA_MAX_DIFF = 0.8
+
+PIXEL_TO_METER = 10e-5
+AREA_TO_METER = 10e-7
+
+DEBUG_ANGLE = 0
 
 
 class Drone():
     def __init__(self, cf, starting_pos=(0,0), starting_height=0.4):
         self.cf = cf
-        self.angle = 0
-        self.height = starting_height
-        self.x = starting_pos[0]
-        self.y = starting_pos[1]
 
-        self.block_pid = PID(0.001, 0.000005, 0.000005, setpoint=0)
+        self.block_pid_x = PID(3, 0.05, 0.01, setpoint=0)
+        self.block_pid_y = PID(3, 0.05, 0.01, setpoint=0)
+        self.block_pid_z = PID(2, 0.05, 0.01, setpoint=IMAGE_CAPTURE_MIN_AREA)
         self.logger = None
         if LOG_TRACKING:
             self.logger = utils.LOGGER(str(int(time.time())) + "_" + str(cf))
@@ -26,6 +29,13 @@ class Drone():
         self.max_iter = 8
         self.found_target = False
         self.last_area = None
+        self.angle = 0
+
+    def get_position(self):
+        return self.cf.position()
+    
+    def get_yaw(self):
+        return self.cf.yaw()
 
     def adjust_drone_position(self, offset_x, offset_y, offset_z):
         """
@@ -49,9 +59,9 @@ class Drone():
                 height = 0.1
         
         if offset_z < 3000:
-            dist = 0.4
+            dist = 0.3
         else:
-            dist = 0.
+            dist = 0.0
 
         return 0, height, dist
 
@@ -64,70 +74,76 @@ class Drone():
         :param offset_z: Area of the target detection rectangle on the frame
         """
         dist_side, height, dist_front, flight_time = 0, 0, 0, STEP_FLIGHT_TIME
-        if not -8 <= offset_x <= 8:
-            # dist_side = round(-1 * self.block_pid(offset_x), 2)
-            if offset_x < 0:
-                dist_side = -0.01
-            elif offset_x > 0:
-                dist_side = 0.01
         
-        if not -30 <= offset_y <= 30:
-            if offset_y < 0:
-                height = -0.1
-            elif offset_y > 0:
-                height = 0.1
+        dist_side = round(-1 * self.block_pid_x(offset_x) * PIXEL_TO_METER, 2)
+            # if offset_x < 0:
+            #     dist_side = -0.01 # max(-0.01, dist_side)
+            # elif offset_x > 0:
+            #     dist_side = 0.01 # min(0.01, dist_side)
         
-        if not IMAGE_CAPTURE_MIN_AREA <= offset_z <= 7000: # 14000:
-            if offset_z < IMAGE_CAPTURE_MIN_AREA:
-                dist_front = 0.05
-                if offset_z < 500: # 1000:
-                    dist_front = 0.1 # 0.3
-                    flight_time *= 1.5
-                elif offset_z < 1500: # 3000:
-                    dist_front = 0.025 # 0.05
-                elif offset_z < 3000: # 6000:
-                    dist_front = 0.015 # 0.025
-                    flight_time /= 2
-                elif offset_z < 4500: # 9000:
-                    dist_front = 0.005 # 0.01
-                    flight_time /= 3
-                elif offset_z < 6000: # 12000:
-                    dist_front = 0.001 # 0.005
-                    flight_time /= 3
+        
+        height = round(-1 * self.block_pid_y(offset_y) * PIXEL_TO_METER, 2)
+            # if offset_y < 0:
+            #     height = -0.05 # max(-0.05, height)
+            # elif offset_y > 0:
+            #     height = 0.05 # min(0.05, height)
 
-            elif offset_z > 7000: # 14000:
-                dist_front = -0.05
+
+
+        
+        
+        dist_front = round(self.block_pid_z(offset_z) * AREA_TO_METER, 2)
+        # safety for not flying to close to the sun
+        if offset_z >= IMAGE_CAPTURE_MIN_AREA:
+            dist_front = min(0, dist_front)
+            # if offset_z < 7000:
+            #     dist_front = 0.03
+                # if offset_z < 500: # 1000:
+                #     dist_front = 0.1 # 0.3
+                #     flight_time *= 1.5
+                # elif offset_z < 1500: # 3000:
+                #     dist_front = 0.025 # 0.05
+                # elif offset_z < 3000: # 6000:
+                #     dist_front = 0.015 # 0.025
+                #     flight_time /= 2
+                # elif offset_z < 4500: # 9000:
+                #     dist_front = 0.005 # 0.01
+                #     flight_time /= 3
+                # elif offset_z < 6000: # 12000:
+                #     dist_front = 0.001 # 0.005
+                #     flight_time /= 3
+
+            # elif offset_z > 7000: # 14000:
+            #     dist_front = -0.05
 
         return dist_side, height, dist_front, flight_time
 
     def move(self, x, y, height, angle, flight_time):
         print("GoTo", x, y, height, angle, flight_time)
-        self.x = x
-        self.y = y
-        self.height = height
-        self.angle = angle
         self.cf.goTo([x, y, height], math.radians(angle), flight_time)
         return flight_time
     
     def move_sideways(self, dist_x, dist_y, flight_time):
         # move closer/further to block
-        self.x += dist_y * math.cos(math.radians(self.angle))
-        self.y += dist_y * math.sin(math.radians(self.angle))
+        _x, _y, _height = self.cf.position()
+        _angle = self.cf.yaw()
+        _x += dist_y * math.cos(_angle)
+        _y += dist_y * math.sin(_angle)
 
         # move side to side
-        angle = self.angle + 90 # drone angle to the left
-        self.x += dist_x * math.cos(math.radians(angle))
-        self.y += dist_x * math.sin(math.radians(angle))
+        angle = _angle + math.radians(90) # drone angle to the left
+        _x += dist_x * math.cos(angle)
+        _y += dist_x * math.sin(angle)
 
-        if self.height < MIN_HEIGHT:
-            self.height = 0.1
+        if _height < MIN_HEIGHT:
+            _height = 0.1
 
         # log everything
         if self.logger is not None:
-            self.logger .log_drone_values(self.x, self.y, self.height, self.angle, flight_time)
+            self.logger .log_drone_values(_x, _y, _height, math.degrees(_angle), flight_time)
         
         # move to position absolute and angle absolute
-        self.cf.goTo([self.x, self.y, self.height], math.radians(self.angle), flight_time)
+        self.cf.goTo([_x, _y, _height], _angle, flight_time)
         return flight_time
 
     def reset_target_condition(self, max_iter=9):
@@ -155,37 +171,40 @@ class Drone():
         if not self.update():
             return None
         # no pallet was seen in this frame move on
-        if pallet_offset is None or not (140 < self.angle < 220):
-            print("No data received, retrying...", self.max_iter, "    ", self.angle)
+        _angle = math.radians(DEBUG_ANGLE) # self.cf.yaw()
+        if pallet_offset is None or  not -20 < abs(math.degrees(_angle)) < 20: # not (160 < abs(math.degrees(_angle)) < 200):
+            print("No data received, retrying...", self.max_iter, "    ", math.degrees(_angle), "   ")
             angle, height, dist, area = 45, 0, 0, 0
             self.max_iter -= 1
+            _angle = math.radians((8 - self.max_iter) * angle)
         else:
             offset_x, offset_y, area, _, _ = pallet_offset
             angle, height, dist = self.adjust_drone_position(offset_x, offset_y, area)
-            
+            _angle += math.radians(angle)
             print("Angle: " + str(angle) + " Height: " + str(height) + " Distance: " + str(dist),
                         "Offsets: x => " + str(offset_x) + " ; y => " + str(offset_y) + " ; area => " + str(area))
         
 
         self.update_target_condition(angle, dist, height, area)
 
-        # update drone movement with new values
-        self.angle += angle
-        self.height += height
+        _x, _y, _height = self.cf.position()
 
-        self.x += dist * math.cos(math.radians(self.angle))
-        self.y += dist * math.sin(math.radians(self.angle))
+        # update drone movement with new values
+        _height += height
+
+        _x += dist * math.cos(_angle)
+        _y += dist * math.sin(_angle)
         flight_time = STEP_FLIGHT_TIME
 
-        if self.height < MIN_HEIGHT:
-            self.height = 0.1
+        if _height < MIN_HEIGHT:
+            _height = 0.3
 
         # log everything
         if self.logger is not None:
-            self.logger .log_drone_values(self.x, self.y, self.height, self.angle, flight_time)
+            self.logger.log_drone_values(_x, _y, _height, math.degrees(_angle), flight_time)
         
         # move to position absolute and angle absolute
-        self.cf.goTo([self.x, self.y, self.height], math.radians(self.angle), flight_time)
+        self.cf.goTo([_x, _y, _height], _angle, flight_time)
         return flight_time
 
     def update_block_search(self, num_pallet_blocks):
@@ -204,23 +223,27 @@ class Drone():
 
         print("Angle: " + str(angle) + " Height: " + str(height) + " Distance: " + str(dist))
            
-        # update drone movement with new values
-        self.angle += angle
-        self.height += height
+        _x, _y, _height = self.cf.position()
+        _angle = self.cf.yaw()
 
-        self.x += dist * math.cos(math.radians(self.angle))
-        self.y += dist * math.sin(math.radians(self.angle))
+
+        # update drone movement with new values
+        _angle += math.radians(angle)
+        _height += height
+
+        _x += dist * math.cos(_angle)
+        _y += dist * math.sin(_angle)
         flight_time = STEP_FLIGHT_TIME
 
-        if self.height < MIN_HEIGHT:
-            self.height = 0.1
+        if _height < MIN_HEIGHT:
+            _height = 0.1
 
         # log everything
         if self.logger is not None:
-            self.logger.log_drone_values(self.x, self.y, self.height, self.angle, flight_time)
+            self.logger.log_drone_values(_x, _y, _height, math.degrees(_angle), flight_time)
         
         # move to position absolute and angle absolute
-        self.cf.goTo([self.x, self.y, self.height], math.radians(self.angle), flight_time)
+        self.cf.goTo([_x, _y, _height], _angle, flight_time)
         return flight_time 
 
     def update_block(self, block_offset):
@@ -256,27 +279,30 @@ class Drone():
             
         
         self.update_target_condition(dist_side, dist_front, height, area)
+
+        _x, _y, _height = self.cf.position()
+        _angle = math.radians(DEBUG_ANGLE) # self.cf.yaw() this introduces an continues offset and rotation
         
-        self.height += height
+        _height += height
 
         # move closer/further to block
-        self.x += dist_front * math.cos(math.radians(self.angle))
-        self.y += dist_front * math.sin(math.radians(self.angle))
+        _x += dist_front * math.cos(_angle)
+        _y += dist_front * math.sin(_angle)
 
         # move side to side
-        angle = self.angle + 90 # drone angle to the left
-        self.x += dist_side * math.cos(math.radians(angle))
-        self.y += dist_side * math.sin(math.radians(angle))
+        angle = _angle + math.radians(90) # drone angle to the left
+        _x += dist_side * math.cos(angle)
+        _y += dist_side * math.sin(angle)
 
-        if self.height < MIN_HEIGHT:
-            self.height = 0.1
+        if _height < MIN_HEIGHT:
+            _height = MIN_HEIGHT
 
         # log everything
         if self.logger is not None:
-            self.logger .log_drone_values(self.x, self.y, self.height, self.angle, flight_time)
+            self.logger.log_drone_values(_x, _y, _height, math.degrees(_angle), flight_time)
         
         # move to position absolute and angle absolute
-        self.cf.goTo([self.x, self.y, self.height], math.radians(self.angle), flight_time)
+        self.cf.goTo([_x, _y, _height], _angle, flight_time)
         return flight_time
 
     def update(self):
---
+++ reid_demo_v2.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+from pycrazyswarm import Crazyswarm
+from MQTTClient import MQTTClient
+from utils import choose_best_bb, choose_middle_bb, choose_closest_bb,  Mode
+from Drone import Drone
+import time
+
+LOG_TRACKING = True
+DRONE_ID = 114
+STARTING_HEIGHT = 0.4
+STEP_FLIGHT_TIME = 1.5
+PALLET_OFFSET_TOPIC = "pallet_bb"
+PALLET_BLOCK_OFFSET_TOPIC = "palletBlock_bb"
+PALLET_BLOCK_CONTINUE_TOPIC = "move_to_next_block"
+
+def main():
+    client = MQTTClient("localhost", 5001, [PALLET_OFFSET_TOPIC, PALLET_BLOCK_OFFSET_TOPIC, PALLET_BLOCK_CONTINUE_TOPIC])
+    swarm = Crazyswarm()
+    timeHelper = swarm.timeHelper
+    allcfs = swarm.allcfs
+
+    cf = allcfs.crazyfliesById[DRONE_ID]
+
+    allcfs.takeoff(targetHeight=STARTING_HEIGHT, duration=4.0)
+    timeHelper.sleep(5.0)
+
+    running = True
+    drone = Drone(cf, (0, 0), STARTING_HEIGHT)
+    current_mode = Mode.PALLET
+    blocks_passed = 0
+
+    x, y, _ = drone.get_position()
+    angle = drone.get_yaw()
+
+    flight_time = drone.move(x, y, STARTING_HEIGHT, angle, 3)
+    last_command_time = time.time()
+    timeHelper.sleep(flight_time)
+
+    flight_start_time = time.time()
+
+    while running:
+
+        # we are looking for the pallet
+        if current_mode == Mode.PALLET:
+
+            # get pallet_offset (offset_x, offset_y, area, center_x, center_y)
+            pallet_offsets = client.get_bb(PALLET_OFFSET_TOPIC)
+            # choose best bb (Problem: Alternating choices)
+            target_offset = choose_best_bb(pallet_offsets)
+            # drone updates position, angle => flighs to position
+            flight_time = drone.update_pallet(target_offset)
+            last_command_time = time.time()
+            timeHelper.sleep(flight_time)
+            
+            # desired distance, height, and angle is reached
+            if drone.check_target_condition():
+                print("Found pallet and have reached desired position. Continueing to next stage...")
+                current_mode = Mode.BLOCK
+                # reset drone search settings
+                drone.reset_target_condition()
+
+
+        elif current_mode == Mode.BLOCK:
+
+            # check if new message is ready or the waiting time has been exceeded
+            while not client.message_ready[PALLET_BLOCK_OFFSET_TOPIC] and last_command_time + flight_time > time.time():
+                timeHelper.sleep(0.1)
+
+
+            pallet_block_offsets = client.get_bb(PALLET_BLOCK_OFFSET_TOPIC)
+            # pallet_block_offsets = filter_pallet_bbs(pallet_block_offsets)
+
+            # select the desired bounding box
+            target_offset = None
+            # if pallet_block_offsets is not None and len(pallet_block_offsets) >= 3:
+            #     target_offset = choose_middle_bb(pallet_block_offsets)
+            # else:
+            if pallet_block_offsets is not None:
+                target_offset = choose_closest_bb(pallet_block_offsets)
+
+            flight_time = drone.update_block(target_offset)
+            last_command_time = time.time()
+            
+            if client.get_bb(PALLET_BLOCK_CONTINUE_TOPIC) is not None:
+                print("Found block", str(blocks_passed), "moving to next...")
+                blocks_passed += 1
+                # reset drone search settings
+                drone.reset_target_condition()
+                if blocks_passed >= 3:
+                    print("Found all blocks returning to base...")
+                    current_mode = Mode.FINISHED
+                    running = False
+                    flight_duration = time.time() - flight_start_time
+                    content = {
+                        "type": "flight_time",
+                        "content": flight_duration
+                    }
+                    client.publish_on_topic("flight_time", content, qos=2)
+                else:
+                    movement = [(0.47, -0.1, 3), (-1.05, -0.1, 5)]
+                    flight_time = drone.move_sideways(*movement[blocks_passed-1])
+                    last_command_time = time.time()
+                    timeHelper.sleep(flight_time)
+
+
+        elif current_mode == Mode.FINISHED:
+            running = False
+
+
+        if flight_time is None:
+            print("Found no pallet rtb...")
+            current_mode = Mode.PALLET
+            drone.reset_target_condition()
+            flight_time = drone.move(0, 0, STARTING_HEIGHT, 0, 4)
+            timeHelper.sleep(flight_time)
+            print("Nothing found. Press Enter to try again...")
+            swarm.input.waitUntilButtonPressed()
+            flight_start_time = time.time()
+            flight_time = 2
+
+
+    # return to base
+    flight_time = drone.move(0, 0, STARTING_HEIGHT, 0, 5)
+    timeHelper.sleep(flight_time)
+    allcfs.land(targetHeight=0.05, duration=3.0)
+    timeHelper.sleep(4)
+
+    client.close()
+
+if __name__ == "__main__":
+    main()
+
